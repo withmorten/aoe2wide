163f7dfa9fea97326addd86b5fb1c24c3538abae
Bart de Boer <boekabart@users.noreply.github.com>
2010-01-20 14:55:45 +0100

    Refactor: Moved UserFeedback stuff to own class. Dump mapping info. Handle 'HV' care more elegantly

--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Security.AccessControl;
-using System.Windows.Forms;
 
 namespace AoE2Wide
 {
@@ -21,18 +18,18 @@ namespace AoE2Wide
         private static string FindPatchFile()
         {
             const string fileName = @"AoE2Wide.dat";
-            DumpTrace(@"Locating Patch data file '{0}'", fileName);
+            UserFeedback.Trace(@"Locating Patch data file '{0}'", fileName);
             var files = Directory.GetFiles(Directory.GetCurrentDirectory(), fileName, SearchOption.AllDirectories);
-            if (files.Length==0)
+            if (files.Length == 0)
                 throw new Exception(@"No AoE2Wide.dat found in current directory or subdirectories");
 
             if (files.Length > 1)
             {
-                DumpWarning(@"Multiple AoE2Wide.dat instances found in current directory and subdirectories:");
-                foreach( var file in files)
-                    DumpTrace(file);
+                UserFeedback.Warning(@"Multiple AoE2Wide.dat instances found in current directory and subdirectories:");
+                foreach (var file in files)
+                    UserFeedback.Trace(file);
             }
-            DumpInfo(@"Using '{0}'", files[0]);
+            UserFeedback.Info(@"Using '{0}'", files[0]);
             return files[0];
         }
 
@@ -45,79 +42,69 @@ namespace AoE2Wide
             }
             catch (Exception e)
             {
-                DumpException(e);
+                UserFeedback.Error(e);
             }
 
-            // Assumption:
-            // If the console window title is equal to the executable path, exe was run by double
-            // If it doesn't (eg. prefixed by c:\windows\system32\cmd.exe - c:\Progra...... )
-            //  it was run from a console window (or batch file!) and shouldn't wait for a key.
-            var runningFromCommandPrompt =
-                !System.Windows.Forms.Application.ExecutablePath.Equals(Console.Title,
-                                                                        StringComparison.InvariantCultureIgnoreCase);
-            if (runningFromCommandPrompt)
-                return;
+            UserFeedback.Close();
+        }
 
-            DumpInfo(@"Press any key to quit");
-            while (!Console.KeyAvailable)
+        static void Go(string[] args)
+        {
+            _patchFileName = FindPatchFile();
+
+            if (!File.Exists(OrgExeName))
+                throw new Exception(string.Format(@"Cannot find original exe '{0}' in current folder", OrgExeName));
+            if (!File.Exists(OrgDrsName))
+                throw new Exception(string.Format(@"Cannot find drs file '{0}' in current folder", OrgDrsName));
+
+            switch (args.Length)
             {
-                System.Threading.Thread.Sleep(50);
+                case 0:
+                    {
+                        UserFeedback.Info(
+                            "Auto patching for all current screen sizes. Note that the game will always use the primary screen!");
+                        var doneList = new HashSet<uint>();
+                        foreach (var screen in System.Windows.Forms.Screen.AllScreens)
+                        {
+                            try
+                            {
+                                var newWidth = (uint)screen.Bounds.Width;
+                                var newHeight = (uint)screen.Bounds.Height;
+                                var key = newWidth + (newHeight << 16);
+                                if (doneList.Add(key))
+                                {
+                                    MakePatch(newWidth, newHeight);
+                                }
+                            }
+                            catch (Exception e)
+                            {
+                                UserFeedback.Error(e);
+                            }
+                        }
+                    }
+                    break;
+                case 2:
+                    {
+                        uint newWidth, newHeight;
+                        if (!uint.TryParse(args[0], out newWidth) || !uint.TryParse(args[1], out newHeight))
+                        {
+                            ShowUsage();
+                            return;
+                        }
+
+                        MakePatch(newWidth, newHeight);
+                    }
+                    break;
+                default:
+                    ShowUsage();
+                    break;
             }
         }
 
-        static void Go( string[] args )
-            {
-                _patchFileName = FindPatchFile();
-
-                if (!File.Exists(OrgExeName))
-                    throw new Exception(string.Format(@"Cannot find original exe '{0}' in current folder", OrgExeName));
-                if (!File.Exists(OrgDrsName))
-                    throw new Exception(string.Format(@"Cannot find drs file '{0}' in current folder", OrgDrsName));
-
-                if (args.Length == 0)
-                {
-                    DumpInfo(
-                        "Auto patching for all current screen sizes. Note that the game will always use the primary screen!");
-                    var doneList = new HashSet<uint>();
-                    foreach (var screen in System.Windows.Forms.Screen.AllScreens)
-                    {
-                        try
-                        {
-                            var newWidth = (uint) screen.Bounds.Width;
-                            var newHeight = (uint) screen.Bounds.Height;
-                            var key = newWidth + (newHeight << 16);
-                            if (doneList.Add(key))
-                            {
-                                MakePatch(newWidth, newHeight);
-                            }
-                        }
-                        catch (Exception e)
-                        {
-                            DumpException(e);
-                        }
-                    }
-                }
-                else if (args.Length == 2)
-                {
-                    uint newWidth, newHeight;
-                    if (!uint.TryParse(args[0], out newWidth) || ! uint.TryParse(args[1], out newHeight))
-                    {
-                        ShowUsage();
-                        return;
-                    }
-
-                    MakePatch(newWidth, newHeight);
-                }
-                else
-                {
-                    ShowUsage();
-                }
-            }
-
         private static void ShowUsage()
         {
-            DumpWarning("Usage: aoe2wide.exe [width height]");
-            DumpInfo("If the new width and height are omitted, the current screen resolution(s) will be used.");
+            UserFeedback.Warning("Usage: aoe2wide.exe [width height]");
+            UserFeedback.Info("If the new width and height are omitted, the current screen resolution(s) will be used.");
         }
 
         private static void MakePatch(uint newWidth, uint newHeight)
@@ -127,111 +114,61 @@ namespace AoE2Wide
                 uint oldWidth, oldHeight;
                 GetOldWidthHeight(newWidth, newHeight, out oldWidth, out oldHeight);
 
-                DumpInfo(string.Format(@"Changing {0}x{1} to {2}x{3}", oldWidth, oldHeight, newWidth, newHeight));
+                UserFeedback.Info(string.Format(@"Changing {0}x{1} to {2}x{3}", oldWidth, oldHeight, newWidth, newHeight));
 
-                DumpTrace(@"Reading original executable");
+                UserFeedback.Trace(@"Reading original executable");
                 var exe = File.ReadAllBytes(OrgExeName);
 
                 var newDrsName = string.Format(@"Data\{0:D4}{1:D4}.drs", newWidth, newHeight);
                 var newExeName = string.Format(@"age2_x1_{0}x{1}.exe", newWidth, newHeight);
 
-                //DumpTrace(@"Writing file with all occurrences of resolutions");
+                //Trace(@"Writing file with all occurrences of resolutions");
                 //Patcher.ListEm(bytes);
 
-                DumpTrace("Reading the patch file");
+                UserFeedback.Trace("Reading the patch file");
                 var patch = Patcher.ReadPatch(_patchFileName);
 
-                DumpTrace("Patching the executable: DRS reference");
+                UserFeedback.Trace("Patching the executable: DRS reference");
                 Patcher.PatchDrsRefInExe(exe, Path.GetFileName(newDrsName));
 
-                DumpTrace("Patching the executable: resolutions");
+                UserFeedback.Trace("Patching the executable: resolutions");
                 Patcher.PatchResolutions(exe, oldWidth, oldHeight, newWidth, newHeight, patch);
 
-                DumpTrace(string.Format(@"Writing the patched executable '{0}'", newExeName));
+                UserFeedback.Trace(string.Format(@"Writing the patched executable '{0}'", newExeName));
                 File.WriteAllBytes(newExeName, exe);
 
-                DumpTrace(@"Opening original interfac.drs");
+                UserFeedback.Trace(@"Opening original interfac.drs");
                 using (
                     var interfaceDrs = new FileStream(OrgDrsName, FileMode.Open,
                                                       FileSystemRights.ReadData,
-                                                      FileShare.Read, 1024*1024, FileOptions.SequentialScan))
+                                                      FileShare.Read, 1024 * 1024, FileOptions.SequentialScan))
                 {
-                    DumpTrace(string.Format(@"Creating patched drs file '{0}'", newDrsName));
+                    UserFeedback.Trace(string.Format(@"Creating patched drs file '{0}'", newDrsName));
                     using (
                         var newDrs = new FileStream(newDrsName, FileMode.Create, FileSystemRights.Write, FileShare.None,
-                                                    1024*1024, FileOptions.SequentialScan))
+                                                    1024 * 1024, FileOptions.SequentialScan))
                     {
-                        DumpTrace(@"Patching DRS");
+                        UserFeedback.Trace(@"Patching DRS");
                         DrsPatcher.Patch(interfaceDrs, newDrs, oldWidth, oldHeight, newWidth, newHeight);
                     }
                 }
 
-                DumpTrace("Done");
+                UserFeedback.Trace("Done");
             }
             catch (Exception e)
             {
-                DumpException(e);
+                UserFeedback.Error(e);
             }
-        }
-
-        private static void DumpException(Exception e)
-        {
-            var normalColor = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("An error occurred:");
-            Console.WriteLine(e.Message);
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.WriteLine("Exception Details:");
-            Console.WriteLine(e.ToString());
-            Console.ForegroundColor = normalColor;
-        }
-
-        private static void DumpInfo(string msg, string param1)
-        {
-            DumpInfo(string.Format(msg, param1));
-        }
-
-        private static void DumpInfo(string msg)
-        {
-            Console.WriteLine(msg);
-        }
-
-        private static void DumpTrace(string msg, string param1)
-        {
-            DumpTrace(string.Format(msg, param1));
-        }
-
-        private static void DumpTrace(string msg)
-        {
-            var normalColor = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.DarkGray;
-            Console.WriteLine(msg);
-            Console.ForegroundColor = normalColor;
-        }
-
-        private static void DumpWarning(string msg, string param1)
-        {
-            DumpWarning(string.Format(msg, param1));
-        }
-
-        private static void DumpWarning(string msg)
-        {
-            var normalColor = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.Write("Warning: ");
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine(msg);
-            Console.ForegroundColor = normalColor;
         }
 
         private static void GetOldWidthHeight(uint newWidth, uint newHeight, out uint oldWidth, out uint oldHeight)
         {
-            var widths = new uint[] {800, 1024, 1280};
-            var heights = new uint[] {600, 768, 1024};
-            
-            for (var q = widths.Length-1; q>=0; q--)
+            var widths = new uint[] { 800, 1024, 1280 };
+            var heights = new uint[] { 600, 768, 1024 };
+
+            for (var q = widths.Length - 1; q >= 0; q--)
             {
-                if (newWidth >= widths[q] && newHeight>= heights[q])
+                if (newWidth >= widths[q] && newHeight >= heights[q])
                 {
                     oldHeight = heights[q];
                     oldWidth = widths[q];
---
+++ src/UserFeedback.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace AoE2Wide
+{
+    internal class UserFeedback
+    {
+        public static void Error(Exception e)
+        {
+            var normalColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("An error occurred:");
+            Console.WriteLine(e.Message);
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Exception Details:");
+            Console.WriteLine(e.ToString());
+            Console.ForegroundColor = normalColor;
+        }
+
+        public static void Info(string msg, string param1)
+        {
+            Info(string.Format(msg, param1));
+        }
+
+        public static void Info(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        public static void Trace(string msg, string param1)
+        {
+            Trace(string.Format(msg, param1));
+        }
+
+        public static void Trace(string msg)
+        {
+            var normalColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine(msg);
+            Console.ForegroundColor = normalColor;
+        }
+
+        public static void Warning(string msg, string param1)
+        {
+            Warning(string.Format(msg, param1));
+        }
+
+        public static void Warning(string msg)
+        {
+            var normalColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("Warning: ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(msg);
+            Console.ForegroundColor = normalColor;
+        }
+
+        internal static void Close()
+        {
+            // Assumption:
+            // If the console window title is equal to the executable path, exe was run by double
+            // If it doesn't (eg. prefixed by c:\windows\system32\cmd.exe - c:\Progra...... )
+            //  it was run from a console window (or batch file!) and shouldn't wait for a key.
+            var runningFromCommandPrompt =
+                !System.Windows.Forms.Application.ExecutablePath.Equals(Console.Title,
+                                                                        StringComparison.InvariantCultureIgnoreCase);
+            if (runningFromCommandPrompt)
+                return;
+
+            Info(@"Press any key to quit");
+            while (!Console.KeyAvailable)
+            {
+                System.Threading.Thread.Sleep(50);
+            }
+        }
+    }
+}
